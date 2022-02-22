30a2b0f8749e913fbcf6f51378dcab69f984b770
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-02-22 10:34:17 +0900

    10th commit

--- app/src/main/java/com/wing/tree/n/back/training/di/FirebaseModule.kt
+++ app/src/main/java/com/wing/tree/n/back/training/di/FirebaseModule.kt
@@ -1,10 +1,8 @@
 package com.wing.tree.n.back.training.di
 
-import android.content.Context
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.wing.tree.n.back.training.data.database.Database
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
